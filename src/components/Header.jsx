233f6aa75a0291b5f423711e9963e2004764b250
rodriguestg <grs.ibr@gmail.com>
2022-06-21 19:32:43 -0300

    Requisito 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ReactClass extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default ReactClass;
