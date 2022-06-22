27cdde8e5aa94c869cd5227496e57f1a81739cde
rodriguestg <grs.ibr@gmail.com>
2022-06-21 21:07:07 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <SolarSystem />
+      <>
+        <SolarSystem />
+        <Missions />
+      </>
     );
   }
 }
